58d96f2f2f3fe0fff197c1ad4c1dc436544bfc26
PritOriginal <72299692+PritOriginal@users.noreply.github.com>
2022-04-05 18:39:24 +0300

    Add files via upload
    tess

--- app/src/main/java/com/example/mysor/server/DownloadImageBitmapTask.kt
+++ app/src/main/java/com/example/mysor/server/DownloadImageBitmapTask.kt
@@ -10,10 +10,21 @@ import com.example.mysor.R
 import java.io.InputStream
 import java.net.URL
 
-class DownloadImageBitmapTask(var mContext : Context) : AsyncTask<String, Void, Bitmap>() {
+class DownloadImageBitmapTask(var mContext : Context, var image : Int, var id_label : Int, var id_photo : Int) : AsyncTask<String, Void, Bitmap>() {
+    constructor(mContext: Context, image: Int) : this(mContext, image, 0, 0)
+    constructor(mContext : Context, id_label : Int, id_photo : Int) : this(mContext, 0 , id_label, id_photo)
+
     override fun doInBackground(vararg urls: String?): Bitmap? {
         val res: Resources = mContext.resources
-        val url = res.getString(R.string.urlImage) + "/repair.png"
+        var url = ""
+        if (image != 0) {
+            when (image) {
+                1 -> url = res.getString(R.string.urlImage) + "/repair.png"
+                2 -> url = res.getString(R.string.urlImage) + "/in.png"
+            }
+        } else {
+            url = res.getString(R.string.urlImage) + "/l_" + id_label + "/new_image" + id_photo + ".png"
+        }
         var urldisplay = url
         var mIcon11 : Bitmap? = null
         try {
